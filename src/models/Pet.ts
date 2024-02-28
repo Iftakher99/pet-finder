0d10716e81ba7532250988f58c1de22f3457b827
iftakher99 <19tonmoy99@gmail.com>
2024-02-28 11:02:46 +0600

    initializing the pet finder project with starter files created json-server backend with user search term function

---
+++ src/models/Pet.ts
@@ -0,0 +1,11 @@
+export interface Pet {
+  id: number;
+  name: string;
+  animalType: 'dog' | 'cat';
+  age: number;
+  description: string;
+  image: string;
+  bread: string;
+  isBoy: boolean;
+  isAdopted: boolean;
+}
